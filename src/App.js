0e9eb0bdf6d21c76790c6c76a29646c6aa973465
AxlRicci <alexbricci@gmail.com>
2020-10-29 05:44:40 -0400

    added react-router-dom, setup app.js for routing. imported normalize.css and sanitize.css into index.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
-  return <div className="App">MEAPP</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <div>Placeholder div</div>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
